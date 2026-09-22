cb206d6ecd705baac3f6404aa52687ea380c68e3
Axel Bueno <elaxelbueno@gmail.com>
2026-09-22 12:12:03 +0200

    dia 71 - pandas

--- ejercicios/dia_71.py
+++ ejercicios/dia_71.py
@@ -1,7 +1,7 @@
 """ 
     Día 71 - Leer CSV con pandas
 
-    Crear un CSV con pandas
+    Crear un CSV:
     dia, actividad, minutos
     lunes, alemán, 60
     lunes, python, 20
@@ -12,3 +12,17 @@
     Extra: mostrar solo activades de alemán
 """
 
+import pandas as pd
+from pathlib import Path
+
+OUTPUT_DIR = Path(__file__).resolve().parent.parent / "outputs"
+nombre = "actividades.csv"
+ruta = OUTPUT_DIR / nombre
+
+# leemos el csv y lo printamos
+df = pd.read_csv(ruta)
+print(df)
+
+# Ahora trabajamos con el dataframe, vamos a mostrar solo las actividades de alemán
+print("Mostramos solo las actividades de alemán:")
+print(df[df["actividad"]=="alemán"])
